680bcc2700c9a7d1cca0e208825f1a8069d315f9
KlaudiaSlawecka <klaudia.slawecka@wp.pl>
2022-02-27 22:40:51 +0100

    Zrobiłam podstawowy wygląd zarządzania w trakcie

--- rpg-frontend/src/app/app.module.ts
+++ rpg-frontend/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ElementsManagementComponent } from './elements-management/elements-management.component';
+import { ManagementDuringGameComponent } from './management-during-game/management-during-game.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ElementsManagementComponent,
+    ManagementDuringGameComponent,
     
   ],
   imports: [
